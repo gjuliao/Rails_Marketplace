7769952721d89f76685411d6c4fd6cbc982c80de
gjuliao <gjuliao32@gmail.com>
2024-05-17 08:08:34 -0500

    Customized routes for sign in and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_up: 'sign_up', sign_in: 'sign_in' }
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
