f51e08d4401a5d096b81753e635f3479a1a86758
gjuliao <gjuliao32@gmail.com>
2024-05-17 08:26:25 -0500

    Added homepage design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_up: 'sign_up', sign_in: 'sign_in' }
   root 'home#index'
+  devise_for :users, path: '', path_names: { sign_up: 'sign_up', sign_in: 'sign_in' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
 end
