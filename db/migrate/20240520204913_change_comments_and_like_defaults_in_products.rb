31950014cbba6b932d334c96e21301c243234552
gjuliao <gjuliao32@gmail.com>
2024-05-20 16:10:38 -0500

    Updated the comment column in schema

--- db/migrate/20240520204913_change_comments_and_like_defaults_in_products.rb
+++ db/migrate/20240520204913_change_comments_and_like_defaults_in_products.rb
@@ -3,5 +3,6 @@ class ChangeCommentsAndLikeDefaultsInProducts < ActiveRecord::Migration[7.1]
     change_column_default :products, :like, from: nil, to: 0
     change_column_default :products, :assistants, from: nil, to: 0
     add_column :products, :comments_counter, :integer, default: 0
+    rename_column :products, :comments, :comment
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_204913) do
     t.string "name"
     t.text "description"
     t.integer "assistants", default: 0
-    t.string "comments"
+    t.string "comment"
     t.integer "like", default: 0
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
