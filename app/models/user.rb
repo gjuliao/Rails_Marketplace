90de836f1b60a09c3122f617c9467bbf1d17660f
gjuliao <gjuliao32@gmail.com>
2024-05-20 20:04:47 -0500

    Edited belongs to in product and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :products
+  has_many :products, foreign_key: 'owner_id'
   has_many :comments, foreign_key: 'author_id'
 end
