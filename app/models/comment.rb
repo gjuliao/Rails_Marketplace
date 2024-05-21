86920b5af96a417c8b8686388eed37cffbe04720
gjuliao <gjuliao32@gmail.com>
2024-05-20 21:03:22 -0500

    Updating the update_products_comments_count method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  belongs_to :product
+  belongs_to :product, counter_cache: :comments_counter
 
   after_create :update_products_comments_count
 
   def update_products_comments_count
-    product.comments_counter = product.comments.count + 1
-    product.save
+    product.comments_counter = product.comments.count
+    if save
+      Rails.logger.info "Product #{product.id} comments_counter updated to #{product.comments_counter}"
+    else
+      Rails.logger.error "Failed to update comments_counter for Product #{product.id}"
+    end
   end
 end
