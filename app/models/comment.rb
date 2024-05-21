08b54fb4cbd67eca843f276ae7e588f81e02d431
gjuliao <gjuliao32@gmail.com>
2024-05-20 21:56:03 -0500

    Updating products comments count still persisting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,22 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  belongs_to :product, counter_cache: :comments_counter
+  belongs_to :product, class_name: 'Product', foreign_key: :product_id
 
   after_create :update_products_comments_count
 
+  # def update_products_comments_count
+  #   Rails.logger.info "Executing after_create callback for Comment #{id}"
+  #   product.reload
+  #   if product.update(comments_counter: product.comments.count)
+  #     Rails.logger.info "Product #{product.id} comments_counter updated to #{product.comments_counter}"
+  #   else
+  #     Rails.logger.error "Failed to update comments_counter for Product #{product.id}"
+  #   end
+  # end
+
+  private
+
   def update_products_comments_count
-    product.comments_counter = product.comments.count
-    if save
-      Rails.logger.info "Product #{product.id} comments_counter updated to #{product.comments_counter}"
-    else
-      Rails.logger.error "Failed to update comments_counter for Product #{product.id}"
-    end
+    product.update(comments_counter: product.comments.count)
   end
 end
