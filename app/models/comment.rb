350505e268faadd878526013f5865c44a83083c1
gjuliao <gjuliao32@gmail.com>
2024-05-20 20:08:15 -0500

    Added foreign key and method to update comments count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :product
+
+  before_create :update_products_comments_count
+
+  private
+
+  def update_products_comments_count
+    product.comments_counter = product.comments.count
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ApplicationRecord
-  belongs_to :owner, class_name: 'User'
+  belongs_to :owner, class_name: 'User', foreign_key: :owner_id
 end
