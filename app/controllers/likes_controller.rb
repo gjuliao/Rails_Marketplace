76cee8a6c1c0dea329c2f2eaf8005db973cf6315
gjuliao <gjuliao32@gmail.com>
2024-05-23 12:04:08 -0500

    Added create method in like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,36 @@
 class LikesController < ApplicationController
+
+  def new
+    @like = Like.new
+  end
+
+  def create
+    @product = Product.find_by(id: params[:product_id])
+    @user = User.find_by(id: params[:user_id])
+    @like = Like.create(like_params)
+    if @like.save
+      flash[:notice] = 'Like saved succesfully'
+      redirect_to user_product_path(@user, @product)
+    else
+      flash.now[:alert] = 'Like not saved.  Please try again'
+      render 'product#show'
+    end
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    if @like.destroy
+      flash[:notice] = 'Like succesfully deleted'
+      # redirect to 'product_path'
+    else
+      flash.now[:alert] = 'Like not deleted.  Please try again'
+      render 'product#show'
+    end
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id, :product_id)
+  end
 end
