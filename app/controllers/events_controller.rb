50ab5f89d451347dc99d9619385b352bcf7dc46b
gjuliao <gjuliao32@gmail.com>
2024-05-25 13:07:01 -0500

    Added form styling to events new view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,10 @@
 class EventsController < ApplicationController
+
+    def new
+        @event = Event.new
+        @product = Product.find(params[:product_id])
+    end
+
+    def create
+    end
 end
