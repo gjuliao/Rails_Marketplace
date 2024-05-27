a56586680dae3c7ded1536052d512cd84fc1e6fb
gjuliao <gjuliao32@gmail.com>
2024-05-26 19:30:45 -0500

    Added new, create & show method in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,31 @@
 class EventsController < ApplicationController
 
     def new
-        @event = Event.new
-        @product = Product.find(params[:product_id])
+      @event = Event.new
+      @product = Product.find(params[:product_id])
     end
 
     def create
+      @event = Event.create(create_params)
+      @product = Product.find(params[:product_id])
+      if @event.save!
+        flash[:notice] = 'Event created successfully!'
+        redirect_to user_product_event_path(current_user, @product, @event)
+      else
+        flash.now[:alert] = 'Event not created successfully!'
+        redirect_to new_user_product_event_path(@product)
+      end
+    end
+
+    def show
+      @event = Event.find(params[:id])
+      @product = Product.find(params[:product_id])
+    end
+
+    private
+
+    def create_params
+      params.require(:event)
+        .permit(:start_date, :end_date, :start_time, :end_time, :total_sits, :product_id)
     end
 end
