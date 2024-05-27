1ab27fe555823fc1b706413060d2f167dc184331
gjuliao <gjuliao32@gmail.com>
2024-05-27 12:30:18 -0500

    Added category model with references to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_145816) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_27_172918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_categories_on_product_id"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
@@ -58,6 +66,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_145816) do
     t.datetime "updated_at", null: false
     t.integer "comments_counter", default: 0
     t.integer "likes_counter", default: 0
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["owner_id"], name: "index_products_on_owner_id"
   end
 
@@ -75,11 +85,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_145816) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "products"
   add_foreign_key "comments", "products"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "events", "products"
   add_foreign_key "events", "users"
   add_foreign_key "likes", "products"
   add_foreign_key "likes", "users"
+  add_foreign_key "products", "categories"
   add_foreign_key "products", "users", column: "owner_id"
 end
