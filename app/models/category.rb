8d843f9daadc08fa669f6a85b2f43b73e92386e2
gjuliao <gjuliao32@gmail.com>
2024-05-27 17:12:12 -0500

    Added category association & collection select with in product view

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,15 @@
 class Category < ApplicationRecord
+  has_many :products
+
+#   DEFAULT_CATEGORIES = %w[Sport Family Art].freeze
+
+#   before_initialize :ensure_default_categories, if: :new_record?
+
+#   private
+
+#   def ensure_default_categories
+#     DEFAULT_CATEGORIES.each do |category|
+#       Category.find_or_create_by(name: category)
+#     end
+#   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,15 @@ class Product < ApplicationRecord
 
   validates :name, :description, :assistants, presence: true
 
+  before_validation :set_default_category, on: :create
+
+  private
+
   def recent_comments
     comments.order(created_at: :desc)
+  end
+
+  def set_default_category
+    self.category ||= Category.find_or_create_by(name: 'Default')
   end
 end
