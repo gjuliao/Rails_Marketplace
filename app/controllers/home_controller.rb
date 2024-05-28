a1870b7cbd9960b8d6d6ac85160b41d8ae8aa523
gjuliao <gjuliao32@gmail.com>
2024-05-28 08:54:37 -0500

    Added ransack to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
-  def index; end
+  def index
+    @q_events = Event.ransack(params[:q])
+    @q_products = Product.ransack(params[:q])
+
+     @events = @q_events.result(distinct: true)
+     @products = @q_products.result(distinct: true)
+
+     @results = @events + @products
+
+     @results = @results.sort_by(&:created_at)
+     @categories = Category.all
+  end
 end
