543679315375e0a6d4d0af8c462167cba56247d1
gjuliao <gjuliao32@gmail.com>
2024-05-29 09:50:53 -0500

    Added booking create new functionality

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,18 +2,19 @@ class BookingsController < ApplicationController
 
     def new
         @booking = Booking.new
+        @event = Event.find(params[:event_id])
     end
 
     def create
         @booking = Booking.create(bookings_params)
-        @event = params[:event_id]
-        @product = params[:product_id]
-        if @booking.save
+        @event = Event.find(params[:event_id])
+        @product = Product.find(params[:product_id])
+        if @booking.save!
             flash[:notice] = 'Booking created succesfully'
             redirect_to root_path
         else
             flash.now[:alert] = 'Booking not created succesfully'
-            redirec_to user_product_path(@event.user_id, @product)
+            render :new
         end
     end
 
@@ -25,6 +26,6 @@ class BookingsController < ApplicationController
     private
 
     def bookings_params
-        params.require(:booking).permit(:no_of_sits, :event_id, :customer_id)
+        params.require(:booking).permit(:no_of_tickets, :event_id, :customer_id)
     end
 end
