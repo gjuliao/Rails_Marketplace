2f914ad3d3d1dd8aec93289a4f07bc2d96af930e
gjuliao <gjuliao32@gmail.com>
2024-05-30 16:13:46 -0500

    Added specs to product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.create!(fname: 'Frank', lname: 'Test', email: 'giotest@gmail.com', password: '1233456') }
+  let(:product) { Product.new(description: 'hello world', assistants: 5, owner_id: user) }
+
+  it 'creates product succesfully' do
+    user = User.create(fname: 'Frank', lname: 'Test', email: 'giotest@gmail.com', password: '1233456')
+    product = Product.create(name: 'sports', description: 'hello world', assistants: 5, owner_id: user.id)
+    expect(product).to be_valid
+  end
+
+  it 'Product not valid, missing user' do
+    product = Product.create(name: 'sports', description: 'hello world', assistants: 5, owner_id: nil)
+    expect(product).to_not be_valid
+  end
+
+  it 'Product not valid, missing name' do
+    product.name = nil
+    expect(product).to_not be_valid
+  end
+
+  it 'Product not valid, missing description' do
+    product.description = nil
+    expect(product).to_not be_valid
+  end
+
+  it 'Product not valid, missing category' do
+    product.category = nil
+    expect(product).to_not be_valid
+  end
+
+  it 'When product is created, category is equal to Default' do
+    let!(:category) { Category.find_or_create_by(name: 'Default') }
+    expect(product.category.name).to eq('Default')
+    expect(product.category).to_not be_nil
+  end
 end
