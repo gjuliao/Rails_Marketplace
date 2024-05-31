b746b2eded2417227db0b9baf72058c97f58ddac
gjuliao <gjuliao32@gmail.com>
2024-05-31 09:57:39 -0500

    Added product comment tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   context 'validations' do
     let(:user) { User.create!(fname: 'Frank', lname: 'Test', email: 'giotest@gmail.com', password: '1233456') }
-    let(:product) 
-      { Product.create!(name: 'Testing', description: 'This is a testing description', assistants: 5, owner_id: user.id ) }
-    let!(:category) { Category.find_or_create_by(name: 'Default') }
+    let(:product) do
+      Product.create!(name: 'Testing', description: 'This is a testing description', assistants: 5, owner_id: user.id)
+    end
+    # let(:category) { Category.find_or_create_by(name: 'Default') }
 
     it 'creates product succesfully' do
       user = User.create(fname: 'Frank', lname: 'Test', email: 'giotest@gmail.com', password: '1233456')
@@ -36,6 +37,33 @@ RSpec.describe Product, type: :model do
     it 'When product is created, category is equal to Default' do
       expect(product.category.name).to eq('Default')
       expect(product.category).to_not be_nil
+    end
+  end
+
+  context 'comments in product validation' do
+    let(:user) { User.create!(fname: 'Frank', lname: 'Test', email: 'giotest@gmail.com', password: '1233456') }
+    let(:product) do
+      Product.create!(name: 'Testing', description: 'This is a testing description', assistants: 5, owner_id: user.id)
+    end
+
+    let(:comment) do
+      Comment.create!(text: 'This is a test comment in product', author_id: user.id, product_id: product.id)
+    end
+
+    before do
+      Comment.create!(text: 'This is a test comment in product', author_id: user.id, product_id: product.id)
+
+      Comment.create!(text: 'This is a test comment in product', author_id: user.id, product_id: product.id)
+
+      Comment.create!(text: 'This is a test comment in product', author_id: user.id, product_id: product.id)
+    end
+
+    it 'Product has a comment' do
+      expect(comment).to be_valid
+    end
+
+    it 'Product has a comment count of 3' do
+      expect(product.comments.count).to eq(3)
     end
   end
 end
