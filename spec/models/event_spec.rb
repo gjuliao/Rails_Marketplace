1a361f10e9c039c425448a8f33eb20934f524dd8
gjuliao <gjuliao32@gmail.com>
2024-06-01 19:39:04 -0500

    Added event is valid test in rspec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'testing event creation' do
+    let(:user) { User.create!(fname: 'mike', lname: 'Yoki', email: 'test@gmail.com', password: '123456') }
+    let(:product) do
+      Product.create!(name: 'Test product', description: 'this is test description', owner_id: user.id, assistants: 5)
+    end
+    let(:event) do
+      Event.create!(
+        start_date: Date.today,
+        end_date: Date.today + 1,
+        start_time: Time.current,
+        end_time: Time.current + 5,
+        product_id: product.id,
+        user_id: user.id
+      )
+    end
+
+      it 'Check if event is valid' do
+        event = Event.create!(
+          start_date: Date.today,
+          end_date: Date.today + 1,
+          start_time: Time.current,
+          end_time: Time.current + 5,
+          product_id: product.id,
+          user_id: user.id
+        )
+        expect(event).to be_valid
+      end
+
+  end
 end
