8642332bcf3432176685fc2f114764974e0a01f7
gjuliao <gjuliao32@gmail.com>
2024-06-04 10:05:38 -0500

    Added get tests requests to product

--- spec/requests/product_spec.rb
+++ spec/requests/product_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe "Products", type: :request do
 
   let(:user) { User.create!(fname: 'Frank', lname: 'Test', email: 'giotest@gmail.com', password: '1233456') }
+
+  let(:product) do
+    Product.create(name: 'Testing', description: 'This is a testing description', assistants: 5, owner_id: user.id)
+  end
+
   before do
     sign_in user
     Product.create(name: 'Testing', description: 'This is a testing description', assistants: 5, owner_id: user.id)
@@ -15,6 +20,16 @@ RSpec.describe "Products", type: :request do
 
   it 'GET users/:user_id/myproducts' do
     get my_products_user_path(user)
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'GET users/:user_id/products/new' do
+    get new_user_product_path(user)
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'GET users/:user_id/products/:product_id/edit' do
+    get edit_user_product_path(user, product)
     expect(response).to have_http_status(:success)
   end
 end
