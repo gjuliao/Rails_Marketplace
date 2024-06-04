2b1081c9fd865c77f0f1277ea25b99c0db5627df
gjuliao <gjuliao32@gmail.com>
2024-06-04 16:03:04 -0500

    Fixed linters

--- spec/requests/product_spec.rb
+++ spec/requests/product_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Products", type: :request do
-
+RSpec.describe 'Products', type: :request do
   let(:user) { User.create!(fname: 'Frank', lname: 'Test', email: 'giotest@gmail.com', password: '1233456') }
 
   let(:product) do
@@ -17,7 +16,7 @@ RSpec.describe "Products", type: :request do
       get root_path
       expect(response).to have_http_status(:success)
     end
-  
+
     it 'GET users/:user_id/myproducts' do
       get my_products_user_path(user)
       expect(response).to have_http_status(:success)
@@ -26,7 +25,7 @@ RSpec.describe "Products", type: :request do
       get new_user_product_path(user)
       expect(response).to have_http_status(:success)
     end
-  
+
     it 'GET users/:user_id/products/:product_id/edit' do
       get edit_user_product_path(user, product)
       expect(response).to have_http_status(:success)
@@ -37,7 +36,7 @@ RSpec.describe "Products", type: :request do
     before do
       sign_in user
     end
-    
+
     it 'create method' do
       product = { name: 'testing', description: 'This is a test description to test', assistants: 5, owner_id: user.id }
       post "/users/#{user.id}/products", params: { user:, product: }
@@ -45,19 +44,21 @@ RSpec.describe "Products", type: :request do
     end
 
     it 'update method' do
-      product = Product.create!(name: 'Testing', description: 'This is a testing description', assistants: 5, owner_id: user.id)
-      product_edit = { name: 'testing', description: 'This is a test description to test', assistants: 5, owner_id: user.id }
+      product = Product.create!(name: 'Testing', description: 'This is a testing description', assistants: 5,
+                                owner_id: user.id)
+      product_edit = { name: 'testing', description: 'This is a test description to test', assistants: 5,
+                       owner_id: user.id }
       patch "/users/#{user.id}/products/#{product.id}", params: { user:, product: product_edit }
       puts response.body
       expect(response).to have_http_status(:redirect)
     end
 
     it 'destroy method' do
-      product = Product.create!(name: 'Testing', description: 'This is a testing description', assistants: 5, owner_id: user.id)
+      product = Product.create!(name: 'Testing', description: 'This is a testing description', assistants: 5,
+                                owner_id: user.id)
       delete "/users/#{user.id}/products/#{product.id}", params: { user:, product: }
       puts response.body
       expect(response).to have_http_status(:redirect)
     end
-
   end
 end
